0ae7fdfbd9dbe7c6a1d74834b4f63bc5fadb5b01
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2024-09-28 11:03:12 -0300

    test: Adding permission validation to folders tests

--- folders/tests.py
+++ folders/tests.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.contrib import auth
 from django.test import TestCase, RequestFactory, Client
 
@@ -44,6 +45,41 @@ class FoldersOperationsTestCase(TestCase):
     views.DeleteFolder.as_view()(request, id=self.folder.id)
     folder_exists = models.Folders.objects.filter(title="Folder 1").exists()
     self.assertFalse(folder_exists)
+
+
+class FoldersValidationTestCase(TestCase):
+  @classmethod
+  def setUpTestData(self):
+    self.client = Client()
+    user = utils.create_default_user()
+    utils.log_in_default_user(self.client)
+    self.client_user = auth.get_user(self.client)
+    self.folder = models.Folders.objects.create(title="Folder 1")
+
+  def setUp(self):
+    utils.log_in_default_user(self.client)
+
+  def test_user_without_permission_should_not_update_folder(self):
+    folder_new_data = {'title': 'Folder 1 New Title'}
+
+    kwargs = {'id': self.folder.id}
+    url = reverse('folders-update', kwargs=kwargs)
+    response = self.client.post(url, folder_new_data)
+
+    updated_folder_exists = models.Folders.objects.filter(title="Folder 1 New Title").exists()
+    
+    self.assertFalse(updated_folder_exists)
+    self.assertEqual(response.status_code, 403)
+
+  def test_user_without_permission_should_not_delete_folder(self):
+    kwargs = {'id': self.folder.id}
+    url = reverse('folders-delete', kwargs=kwargs)
+    response = self.client.post(url)
+
+    deleted_folder_still_exists = models.Folders.objects.filter(title="Folder 1").exists()
+    
+    self.assertTrue(deleted_folder_still_exists)
+    self.assertEqual(response.status_code, 403)
 
 class FoldersAutocompleteTestCase(TestCase):
   @classmethod
