d564969059d77d53690eacd82355fd78467409b6
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2024-10-18 11:33:36 -0300

    refactor: Changing note cards layout to asymetrical

--- core/templatetags/utils.py
+++ core/templatetags/utils.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter()
 def filter_current_user_like(value, user):
   return value.filter(user=user).exists()
+
+@register.filter()
+def rest(value1, value2):
+  return value1 % value2
